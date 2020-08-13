9a1385d75a258c491f7de1f21826ba40bf7c3d3b
David Collom <dcollom@lco.global>
2020-08-12 17:35:26 -0700

    Added setup file

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='skip-django',
+    description='Django Plotly Dash app for SCiMMA-Skip',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://scimma.org',
+    author='SCiMMA',
+    author_email='dcollom@lco.global',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    keywords=['scimma', 'astronomy', 'astrophysics', 'cosmology', 'science', 'fits', 'observatory'],
+    packages=find_packages(),
+    use_scm_version=True,
+    setup_requires=['setuptools_scm', 'wheel'],
+    install_requires=[
+        'django==3.0.7'
+        'django_plotly_dash==1.4.2'
+        'dash-bootstrap-components==0.10.3'
+        'whitenoise==5.1.0'
+        'dpd-static-support'
+    ],
+    extras_require={
+    },
+    include_package_data=True,
+)
